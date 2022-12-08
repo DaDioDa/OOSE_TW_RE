151da7f0e2b208e57d7acb648497900edbf8fac3
yuyudess <A11023019@gemail.yuntech.edu.tw>
2022-12-08 21:52:15 +0800

    buttonId

--- app/src/main/java/com/nini/menu/fragmentDrinks.java
+++ app/src/main/java/com/nini/menu/fragmentDrinks.java
@@ -4,13 +4,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
 import androidx.fragment.app.Fragment;
 
 public class fragmentDrinks extends Fragment {
 
+    Button BtnArr[] = new Button[11];
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_drinks, container, false);
+
+        for (int i = 0; i < 11; i++) {
+            int id = getResources().getIdentifier("drinks_"+ i, "id","com.nini.menu");
+            BtnArr[i] = rootView.findViewById(id);
+            BtnArr[i].setText("This is drinks_Button NO." + (i+1));
+        }
 
         return rootView;
     }
